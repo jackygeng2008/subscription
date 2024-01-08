0147dac0909470bbd309f8d16ea81b8c633134a5
jackygeng2008 <jackygeng2008@gmail.com>
2024-01-08 18:00:23 +0800

    Update com.jf.lkrj.ts
    Signed-off-by: jackygeng2008 <jackygeng2008@gmail.com>

--- src/apps/com.jf.lkrj.ts
+++ src/apps/com.jf.lkrj.ts
@@ -5,21 +5,30 @@ export default defineAppConfig({
   name: '花生日记',
   groups: [
     {
-      key: 1,
       name: '首页❌广告',
-      activityIds: ['com.jf.lkrj.MainActivity', 'com.jf.lkrj.SplashActivity'],
+      key: 1,
+      activityIds: [
+        'com.jf.lkrj.MainActivity',
+        'com.jf.lkrj.SplashActivity'
+      ],
       rules: [
         {
-          matches: '[id$="/close_iv"]',
-          snapshotUrls: 'https://i.gkd.li/snapshot/1704701268491',//获取链接的方法在上一步有说明
+          matches: [
+            '[id$="/close_iv"]'
+          ],
+          snapshotUrls: [
+            'https://i.gkd.li/import/13857598'
+          ]
         },
         {
-          matches: '[id$="/cancel_iv"]',
-          snapshotUrls: [
-            'https://i.gkd.li/snapshot/1704702664467',
+          matches: [
+            '[id$="/cancel_iv"]'
           ],
-        },
-      ],
-    },
-  ],
+          snapshotUrls: [
+            'https://i.gkd.li/import/13857788'
+          ]
+        }
+      ]
+    }
+  ]
 }
